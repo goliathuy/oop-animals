fb16d259bc9bef9dc4b9b590f012d92b8911a588
Mario <7848929+goliathuy@users.noreply.github.com>
2023-04-27 23:42:33 -0300

    Missing Parameters modifications

--- src/Utils/Parameters.php
+++ src/Utils/Parameters.php
@@ -1,27 +1,27 @@
 <?php
 namespace Utils;
 
+use InvalidArgumentException;
+use UnexpectedValueException;
+use LogicException;
+
 class Parameters
 {
-    private static function animalParm($name, $type) {
-        return [
-            'name' => $name,
-            'type' => $type
-        ];
-    }
+    /**2DO: move form here */
+    public static $programName = "OOPAnimals";
 
-    public static function examples() {
+    public static function usageExample() {
         global $argc, $argv;
         $scrptName = isset($argc) ? $argv[0] : "script.php";
 
-        $example = "OOPAnimals \n\n".
+        $example = self::$programName . " \n\n".
             "Usage: php $scrptName [options] | <animal_name> <animal_type>\n".
             "Options:\n".
             "  -h, --help\t\tDisplay this help message\n".
             "  -n, --name\t\tAnimal name\n".
             "  -t, --type\t\tAnimal type\n".
             "Description\n".
-            "  OOPAnimals requires a name and an animal type and respond with an appropriate message\n".
+            "  " . self::$programName . " requires a name and an animal type and respond with an appropriate message\n".
             "  It can takes name and type as possitional parameters or you can set it with options.\n".
             "Example:\n".
             " php $scrptName \"Mr Pickles\" cat\n".
@@ -29,60 +29,96 @@ class Parameters
             " php $scrptName --name=\"Mr Pickles\" --type=cat\n";
         return $example;
     }
-    
-    public static function get(){
-        global $argc, $argv;
-        $animals = []; /** name and an animal type. */
 
-        if (isset($argc)) {
-            switch ($argc) {
-                case 1:
-                    //No parameter
-                    break;
-                case 2:
-                    //Positional parameter name, animal type missing
-                    break;
-                case 3:
-                    //Positional parameter name and animal type
-                    break;
-                        
-                default:
-                    # code...
-                    break;
-            }
-//            if($argc  3){
+    public static function commandLineMessage() {
+        return self::$programName . " is setup to work as a command line application";
+    }
 
-            if($argc == 3){
-                $animals[] = self::animalParm($argv[1], $argv[2]);
-            } else {
-                $shortopts = "h::";  // Animal Name
-                $shortopts = "n::";  // Animal Name
-                $shortopts .= "t::"; // Animal Type
-                
-                $longopts  = array(
-                    "name::", //Animal Name
-                    "type::" //Animal Type
-                );
-                $options = getopt($shortopts, $longopts);
+    private static function animalParm($name, $type) {
+        return [
+            'name' => $name,
+            'type' => $type
+        ];
+    }
+
+    private static function getOptions() {
+        $shortopts = "n::";  // Animal Name
+        $shortopts .= "t::"; // Animal Type
+        $shortopts .= "h";  // Show Help
         
-                $animals["IS SET"] = true;
-                //$i = 0 always bing the script name
-                for ($i = 1; $i < $argc; $i++) {
-                    $animals["Argument #" . $i] = $argv[$i];
-                }
-                //$val = getopt("p:");
-                //$val = getopt(null, ["name:"]);
-            }/*
-            for ($i = 0; $i < $argc; $i++) {
-                echo "Argument #" . $i . " - " . $argv[$i] . "\n";
-            }*/
-        }
+        $longopts  = [
+            "name::", //Animal Name
+            "type::", //Animal Type
+            "help" //Show Help
+        ];
+        return getopt($shortopts, $longopts);
+    }
 
-        $animals['getopt'] = $options;
+    private static function pairAnimals($names, $types) {
+        $animals = [];
+        $namesArray = !is_array($names) ? [$names] : $names;
+        $typesArray = !is_array($types) ? [$types] : $types;
+
+        foreach ($namesArray as $key => $name) {
+            if (isset($typesArray[$key])) {
+                $animals[] = self::animalParm($name,$typesArray[$key]);
+            } else {
+                throw new UnexpectedValueException(self::usageExample(), 1);
+            }
+        }
 
         return $animals;
     }
 
+    public static function get() {
+        global $argc, $argv;
+        $failed = false;
+        $animals = []; /** name and an animal type. Or return empty*/
+
+        //Check if i'm in command line
+        if (isset($argc)) {
+
+            $options = self::getOptions();
+
+            if (isset($options['h']) || isset($options['help'])) {
+                return false;
+            } elseif ($argc < 3) {
+                //Missing parameters
+                throw new InvalidArgumentException(self::usageExample(), 1);
+            }
+            
+            /* Short options */
+            if (isset($options['n']) || isset($options['t'])) {
+                if (isset($options['n']) && isset($options['t'])) {
+                    $animals = self::pairAnimals($options['n'], $options['t']);
+                } else {
+                    $failed = true;
+                }
+            } else {
+                /* Long options */
+                if (isset($options['name']) || isset($options['type'])) {
+                    if (isset($options['name']) || isset($options['type'])) {
+                        $animals = self::pairAnimals($options['name'], $options['type']);
+                    } else {
+                        $failed = true;
+                    }
+                } else {
+                    //Must recive name and type in that order.
+                    //Any other parameter will be discarded
+                    $animals[] = self::animalParm($argv[1], $argv[2]);
+                }
+            } 
+        } else {
+            throw new LogicException(self::commandLineMessage(), 1);
+        }
+
+        if ($failed) {
+            //Parameters error
+            throw new UnexpectedValueException(self::usageExample(), 1);
+        }
+
+        return $animals;
+    }
 }
 
 ?>
